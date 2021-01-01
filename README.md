86f0f12c2c6a70a130b61db3bc08f04fb00846af
Nakano Masaki <namachan10777@gmail.com>
2021-01-01 16:19:33 +0900

    add comments

---
+++ README.md
@@ -0,0 +1,8 @@
+# ML from scratch
+numpyとpandas縛り(numpyは固有値計算ぐらいまでは使っても良い）
+
+# scripts
+|script               |description                     |
+|:--------------------|:-------------------------------|
+|fetch\_dataset.sh    | データセットの取得             |
+|lsm\_wine\_quality.py| 最小二乗法重回帰(wine\_quality)|
--- lsm_wine_quality.py
+++ lsm_wine_quality.py
@@ -6,14 +6,17 @@ import numpy.linalg as lin
 
 df = pd.read_csv("./dataset/wine_quality/winequality-red.csv", sep=";")
 
+# 前半2/3を訓練データ、後半1/3をテストデータに使う
 df_train = df[:int(len(df)/3*2)]
 df_test = df[int(len(df)/3*2):]
 
+# qualityを予測する
 target = "quality"
 features = ["fixed acidity", "volatile acidity", "citric acid", "residual sugar", "chlorides", "free sulfur dioxide", "total sulfur dioxide", "density", "pH", "sulphates", "alcohol"]
 
 y = df_train[target].to_numpy().astype(np.float64)
 g = df_train[features].to_numpy()
+# 切片分を挿入
 g = np.insert(g, 0, 1, axis=1)
 a = lin.inv(g.T @ g) @ g.T @ y
 
@@ -21,5 +24,6 @@ y_test = df_test[target].to_numpy().astype(np.float64)
 g_test = df_test[features].to_numpy()
 g_test = np.insert(g_test, 0, 1, axis=1)
 
+# 予測してR^2を計算
 r2 = 1 - np.sum(np.power(y_test - g_test @ a, 2)) / np.sum(np.power(y - np.mean(y), 2))
 print(r2)
