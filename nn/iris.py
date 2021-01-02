17b2212e4fa1ae9f2674c84bc8c315a28007ad0f
Nakano Masaki <namachan10777@gmail.com>
2021-01-02 23:35:48 +0900

    wakaran

--- nn/iris.py
+++ nn/iris.py
@@ -1,39 +1,32 @@
 import nn
 import pandas as pd
 import numpy as np
-df = pd.read_csv("./dataset/iris/iris.data", header=None).to_numpy()
-x_train = np.zeros([90,4])
-y_train = np.empty(90, dtype=object)
-x_train[0:30,:] = df[0:30, :4]
-y_train[0:30] = np.full(30, 0)
-x_train[30:60,:] = df[50:80, :4]
-y_train[30:60] = np.full(30, 1)
-x_train[60:90,:] = df[100:130, :4]
-y_train[60:90] = np.full(30, 2)
 
-x_test = np.zeros([60,4])
-y_test = np.empty(60, dtype=object)
-x_test[0:20,:] = df[30:50, :4]
-y_test[0:20] = np.full(20, 0)
-x_test[20:40,:] = df[80:100, :4]
-y_test[20:40] = np.full(20, 1)
-x_test[40:60,:] = df[130:150, :4]
-y_test[40:60] = np.full(20, 2)
+df = pd.read_csv("./dataset/iris/iris.data", header=None)
+y = df.iloc[0:100,4].values
+y = np.where(y=='Iris-setosa',-1,1)
+X = df.iloc[0:100,[0,1,2,3]].values
 
-train = []
-test = []
-for i in range(90):
-    y = np.zeros(3)
-    y[y_train[i]] = 1.0
-    train.append((x_train[i,:], y))
+X_train = np.empty((80,4))
+X_test = np.empty((20,4))
+y_train = np.empty(80)
+y_test = np.empty(20)
+X_train[:40],X_train[40:] = X[:40],X[50:90]
+X_test[:10],X_test[10:] = X[40:50],X[90:100]
+y_train[:40],y_train[40:] = y[:40],y[50:90]
+y_test[:10],y_test[10:] = y[40:50],y[90:100]
 
-for i in range(60):
-    y = np.zeros(3)
-    y[y_test[i]] = 1.0
-    test.append((x_test[i,:], y))
+print(X_train)
+print(X_test)
+print(y_train)
+print(y_test)
 
-layers = [2]
-bs, ws = nn.train(3.0, train, layers, 1000)
-for (x,y) in test:
-    hs, _ = nn.run(ws, bs, x)
-    print(y, hs[-1])
+w = np.ones(4)/10
+b = np.ones(1)/10
+
+for _ in range(100):
+    w, b = nn.update(X_train, y_train, w, b, eta=0.1)
+    print(f'acc {nn.accuracy(X_test, y_test, w, b)} loss {nn.loss(X_train, y_train, w, b)}')
+
+print(w)
+print(b)
--- nn/nn.py
+++ nn/nn.py
@@ -8,8 +8,20 @@ def sigmoid(x):
 def activation(X, w, b):
     return sigmoid(np.dot(X,w)+b)
 
-def sigmoid_d(x):
-    return (1-sigmoid(x))*sigmoid(x)
+def loss(X, y, w, b):
+    dif = y - activation(X, w, b)
+    return np.sum(dif**2/(2*len(y)), keepdims=True)
 
-def loss(x):
-    return np.sum(np.power(y - activation(X, w, b))/(2*len(y)), keepdims=True)
+def predict(X, w, b):
+    return np.where(activation(X, w, b)<0.5, -1.0, 1.0)
+
+def accuracy(X, y, w, b):
+    pre = predict(X, w, b)
+    return np.sum(np.where(pre==y,1,0))/len(y)
+
+def update(X, y, w, b, eta):
+    a = (activation(X,w,b)-y)*activation(X, w, b)*(1-activation(X,w,b))
+    a = a.reshape(-1,1)
+    w -= eta * 1/float(len(y))*np.sum(a*X,axis=0)
+    b -= eta * 1/float(len(y))*np.sum(a)
+    return w,b
